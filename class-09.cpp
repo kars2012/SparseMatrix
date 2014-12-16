255952e8d28be507c1b2de0ca76839256a2ba064
Bryan Kars <kars2012@gmail.com>
2014-12-15 23:25:59 -0500

    first commit

---
+++ class-09.cpp
@@ -0,0 +1,9 @@
+//
+//  class-09.cpp
+//  proj09
+//
+//  Created by Bryan J Kars on 11/14/14.
+//  Copyright (c) 2014 Bryan J Kars. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ class-09.h
@@ -0,0 +1,13 @@
+//
+//  class-09.h
+//  proj09
+//
+//  Created by Bryan J Kars on 11/14/14.
+//  Copyright (c) 2014 Bryan J Kars. All rights reserved.
+//
+
+#ifndef proj09_class_09_h
+#define proj09_class_09_h
+
+
+#endif
